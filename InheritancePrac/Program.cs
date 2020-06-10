d7b682cc97995a67484aaf60c19c7b73ae26668f
Gihun Ham <progh2@gmail.com>
2020-06-10 14:01:37 +0900

    Dog, Cat 클래스 생성

---
+++ InheritancePrac/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dog> dogs = 
+                new List<Dog> { new Dog(), new Dog(), new Dog() };
+            List<Cat> cats =
+                new List<Cat> { new Cat(), new Cat(), new Cat() };
+
+            foreach( Dog dog in dogs)
+            {
+                dog.Eat();
+                dog.Sleep();
+                dog.Bark();
+            }
+            foreach (Cat cat in cats)
+            {
+                cat.Eat();
+                cat.Sleep();
+                cat.Meow();
+            }
+        }
+    }
+}
